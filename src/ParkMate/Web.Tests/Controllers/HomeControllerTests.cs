4c831817f2a33a9b0e3912d621c53aa26b336b17
Timon Haringa <Timon Haringa>
2019-03-09 12:58:26 +0700

    Commit initial Web and Web.Tests project stubs

---
+++ src/ParkMate/Web.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+using ParkMate.Web.Controllers;
+
+namespace ParkMate.Web.Tests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void IndexWillReturnViewResult()
+        {
+            var sut = new HomeController();
+
+            var result = sut.Index();
+
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
