eaa5c8b75f9a50a22a88f2aadb8ff0437d377067
Benjamin <Benjamin-S@users.noreply.github.com>
2019-03-24 23:24:58 +0800

    Email sender stub

--- src/ParkMate/Infrastructure/Services/EmailSender.cs
+++ src/ParkMate/Infrastructure/Services/EmailSender.cs
@@ -1,13 +1,35 @@
 using System.Threading.Tasks;
 //using ParkMate.ApplicationCore.Interfaces;
 using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+
 namespace ParkMate.Infrastructure.Services
 {
     public class EmailSender : IEmailSender
     {
+        private string sendGridApiKey = null;
+        public IConfiguration Configuration { get; }
+
+        public EmailSender(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            sendGridApiKey = Configuration["SendGrid:ServiceApiKey"];
+        }
+
         public async Task SendEmailAsync(string emailAddress, string subject, string body)
         {
-            throw new System.NotImplementedException();
+            var client = new SendGridClient(sendGridApiKey);
+
+            EmailAddress fromEmail = new EmailAddress("mail@parkmate.com");
+            EmailAddress toEmail = new EmailAddress(emailAddress);
+
+            var msg = MailHelper.CreateSingleEmail(fromEmail, toEmail, subject, null, body);
+
+            var response = await client.SendEmailAsync(msg);
         }
     }
 }
