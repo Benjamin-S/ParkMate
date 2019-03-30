1d39f4e3e1b4db41d71f205ce2fdda0f87292ee1
Benjamin <benjaminsymons@hotmail.com>
2019-03-30 18:15:54 +0800

    Revert to old code

--- src/ParkMate/Infrastructure/Services/EmailSender.cs
+++ src/ParkMate/Infrastructure/Services/EmailSender.cs
@@ -10,23 +10,21 @@ namespace ParkMate.Infrastructure.Services
     public class EmailSender : IEmailSender
     {
         private string _sendGridApiKey = null;
-        private ISendGridClient _emailClient;
 
-        public EmailSender(IConfiguration configuration, ISendGridClient emailClient)
+        public EmailSender(IConfiguration configuration)
         {
             _sendGridApiKey = configuration["SendGrid:ServiceApiKey"];
-            _emailClient = emailClient;
         }
 
         public async Task SendEmailAsync(string emailAddress, string subject, string body)
         {
-
+            var client = new SendGridClient(_sendGridApiKey);
             var fromEmail = new EmailAddress("mail@parkmate.com");
             var toEmail = new EmailAddress(emailAddress);
 
             var msg = MailHelper.CreateSingleEmail(fromEmail, toEmail, subject, null, body);
 
-            var response = await _emailClient.SendEmailAsync(msg);
+            var response = await client.SendEmailAsync(msg);
         }
     }
 }
