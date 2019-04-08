78f33393bd4a4513e13ea1b6a274f0987284ffa8
tharinga <timonster@gmail.com>
2019-04-08 12:04:45 +0700

    Create Customer and Vehicle Entities

---
+++ src/ParkMate/ApplicationCore/Entities/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ParkMate.ApplicationCore.Entities
+{
+    public class Customer
+    {
+        private Customer()
+        {
+        }
+
+        public Customer(string identityId, string phoneNumber, string email)
+        {
+            IdentityId = !string.IsNullOrWhiteSpace(identityId) ?
+                identityId : throw new ArgumentNullException(nameof(identityId));
+            PhoneNumber = !string.IsNullOrWhiteSpace(phoneNumber) ?
+                phoneNumber : throw new ArgumentNullException(nameof(phoneNumber));
+            Email = !string.IsNullOrWhiteSpace(email) ?
+                email : throw new ArgumentNullException(nameof(email));
+        }
+
+        public string IdentityId { get; private set; }
+        public string PhoneNumber { get; private set; }
+        public string Email { get; private set; }
+        public List<Vehicle> Vehicles { get; private set; } = new List<Vehicle>();
+        public List<ParkingSpace> ParkingSpaces { get; private set; } = new List<ParkingSpace>();
+        //public List<Booking> Bookings { get; private set; }
+    }
+}
