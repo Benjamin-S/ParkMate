f4e5c0eec50d677ca7e1c271273a49741fe54c47
aidunn <38102070+aidunn@users.noreply.github.com>
2019-04-10 01:05:30 +1000

    Update Data Annotations to be more readable

--- src/ParkMate/Web/Models/AddressDTO.cs
+++ src/ParkMate/Web/Models/AddressDTO.cs
@@ -5,26 +5,32 @@ namespace ParkMate.Web.Models
     public class AddressDTO
     {
         [Required]
-        [StringLength(50)]
+        [StringLength(255)]
         [Display(Name = "Street Address")]
         public string Street { get; set; }
+
         [Required]
-        [StringLength(50)]
+        [StringLength(255)]
         [Display(Name = "City")]
         public string City { get; set; }
+
         [Required]
-        [StringLength(50)]
+        [StringLength(255)]
         [Display(Name = "State")]
         public string State { get; set; }
+
         [Required]
         [Range(1000,9999)]
         [Display(Name = "Postcode")]
         public string Zip { get; set; }
+
         [Required]
         [Range(-180.999999999, 180.999999999)]
         public double Latitude { get; set; }
+
         [Required]
         [Range(-180.999999999, 180.999999999)]
         public double Longitude { get; set; }
+
     }
 }
--- src/ParkMate/Web/Models/AvaliableDaysDTO.cs
+++ src/ParkMate/Web/Models/AvaliableDaysDTO.cs
@@ -7,21 +7,27 @@ namespace ParkMate.Web.Models
         [Required]
         [Display(Name = "Monday")]
         public bool IsAvailableMonday { get; set; } = true;
+
         [Required]
         [Display(Name = "Tuesday")]
         public bool IsAvailableTuesday { get; set; }
+
         [Required]
         [Display(Name = "Wednesday")]
         public bool IsAvailableWednesday { get; set; }
+
         [Required]
         [Display(Name = "Thursday")]
         public bool IsAvailableThursday { get; set; }
+
         [Required]
         [Display(Name = "Friday")]
         public bool IsAvailableFriday { get; set; }
+
         [Required]
         [Display(Name = "Saturday")]
         public bool IsAvailableSaturday { get; set; }
+
         [Required]
         [Display(Name = "Sunday")]
         public bool IsAvailableSunday { get; set; }
