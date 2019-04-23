999a0988ac5cfd1290b80c08d48662d0f18a1952
aidunn <38102070+aidunn@users.noreply.github.com>
2019-04-23 18:03:39 +1000

    Preliminary Google Maps integreation

--- src/ParkMate/Web/Controllers/SearchController.cs
+++ src/ParkMate/Web/Controllers/SearchController.cs
@@ -36,6 +36,11 @@ namespace ParkMate.Web.Controllers
         {
             var query = new FindSpacesWithinDistanceQuery(dto);
             var result = await _mediator.Send(query);
+
+            foreach (var space in result.Payload)
+            {
+                Console.WriteLine(space.Title);
+            }
             return SearchResult(new SearchResultViewModel()
             {
                 PrevInput = dto,
