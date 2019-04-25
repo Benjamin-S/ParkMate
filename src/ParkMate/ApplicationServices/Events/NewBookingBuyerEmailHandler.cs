2d7a182b6a6c5dc5348883a62ba52360819c2ce7
tharinga <timonster@gmail.com>
2019-04-25 13:50:43 +0700

    Add BookingViewModel to Read Store

--- src/ParkMate/ApplicationServices/Events/NewBookingBuyerEmailHandler.cs
+++ src/ParkMate/ApplicationServices/Events/NewBookingBuyerEmailHandler.cs
@@ -6,12 +6,12 @@ using ParkMate.ApplicationServices.Interfaces;
 
 namespace ParkMate.ApplicationServices.Events
 {
-    public class NewBookingOwnerEmailHandler :
+    public class NewBookingBuyerEmailHandler :
         INotificationHandler<NewBookingCreatedEvent>
     {
         private IEmailService _emailSender;
 
-        public NewBookingOwnerEmailHandler(IEmailService emailSender)
+        public NewBookingBuyerEmailHandler(IEmailService emailSender)
         {
             _emailSender = emailSender;
         }
@@ -20,13 +20,10 @@ namespace ParkMate.ApplicationServices.Events
             NewBookingCreatedEvent notification,
             CancellationToken cancellationToken = default(CancellationToken))
         {
-            /*
-            await _emailSender.SendEmailAsync(notification.ParkingSpace.Owner.Email,
-                "You have a new ParkMate booking",
-                $"Dear {notification.ParkingSpace.Owner.Name},\n\n" +
-                "You have a new ParkMate booking....");
-                */               
-
+            await _emailSender.SendEmailAsync(notification.Buyer.Email,
+                "Your ParkMate Booking",
+                $"Dear {notification.Buyer.Name},\n\n" +
+                "You have succesfully booked....");
         }
     }
 }
