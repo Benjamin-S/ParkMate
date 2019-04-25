85a11f416bf04b888e0f5f61574384aeeb461255
tharinga <timonster@gmail.com>
2019-04-25 18:53:33 +0700

    Add VehicleViewModel

---
+++ src/ParkMate/ApplicationServices/DTOs/VehicleViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ParkMate.ApplicationServices.DTOs
+{
+    public class VehicleViewModel
+    {
+        public int VehicleId { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Colour { get; set; }
+        public string Registration { get; set; }
+    }
+}
