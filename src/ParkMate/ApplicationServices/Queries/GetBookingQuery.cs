dcfa38da54816874e5a8de82b385191d8a209253
tharinga <timonster@gmail.com>
2019-04-28 16:32:22 +0700

    Add GetBookingQuery

---
+++ src/ParkMate/ApplicationServices/Queries/GetBookingQuery.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using MongoDB.Driver;
+using ParkMate.ApplicationServices.Interfaces;
+using ParkMate.ApplicationServices.DTOs;
+
+namespace ParkMate.ApplicationServices.Queries
+{
+    public class GetBookingQuery
+        : IRequest<Result<BookingViewModel>>
+    {
+        public GetBookingQuery(int bookingId)
+        {
+            BookingId = bookingId;
+        }
+        public int BookingId { get; }
+    }
+
+    public class GetBookingQueryHandler
+        : IRequestHandler<GetBookingQuery, Result<BookingViewModel>>
+    {
+        private IMongoContext _context;
+
+        public GetBookingQueryHandler(IMongoContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Result<BookingViewModel>> Handle(
+            GetBookingQuery query,
+            CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var booking = await _context.Bookings
+                .FindAsync(b => b.BookingId == query.BookingId)
+                .Result.FirstOrDefaultAsync();
+
+            if (booking != null)
+            {
+                return Result<BookingViewModel>.QuerySuccess(booking);
+            }
+            return Result<BookingViewModel>.QueryFail("Booking not found");
+        }
+    }
+}
