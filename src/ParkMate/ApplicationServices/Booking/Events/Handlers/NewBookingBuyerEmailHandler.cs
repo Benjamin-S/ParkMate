c5ab94b083f7406e840eae1c0dc9b4ae42717fbf
tharinga <timonster@gmail.com>
2019-05-09 16:08:31 +0700

    Implement booking ermail handlers

--- src/ParkMate/ApplicationServices/Booking/Events/Handlers/NewBookingBuyerEmailHandler.cs
+++ src/ParkMate/ApplicationServices/Booking/Events/Handlers/NewBookingBuyerEmailHandler.cs
@@ -1,5 +1,6 @@
 using System.Threading;
 using System.Threading.Tasks;
+using ApplicationCore.Enums;
 using MediatR;
 using ParkMate.ApplicationCore.Interfaces;
 using ParkMate.ApplicationServices.Interfaces;
@@ -20,10 +21,35 @@ namespace ParkMate.ApplicationServices.Events
             NewBookingCreatedEvent notification,
             CancellationToken cancellationToken = default(CancellationToken))
         {
+            var title = notification.Booking.ParkingSpace.Description.Title;
+            var booking = notification.Booking.BookingInfo;
+            string units = notification.Booking.BookingInfo.BillingUnit == BillingUnit.Hourly ? "Hours" : "Days";
+
             await _emailSender.SendEmailAsync(notification.Buyer.Email,
-                "Your ParkMate Booking",
-                $"Dear {notification.Buyer.Name},\n\n" +
-                "You have succesfully booked....");
-        }
+                $"New booking for {title} successful",
+                $"Dear {notification.Buyer.Name}," +
+                $"<br>Congratulations! Your booking for {title} has been confirmed. " +
+                    "Please see below the details of the booking:<br>" +
+                    "<br>&nbsp;Booking Reference: " + notification.Booking.Id +
+                    "<br>&nbsp;ParkingSpace Name: " + title +
+                    "<br>&nbsp;Description: " + notification.Booking.ParkingSpace.Description.Description +
+                    "<br>&nbsp;Time of Booking: " + booking.Start.ToString("dddd, dd MMMM yyyy") +
+                    "<br>&nbsp;Owner: " + notification.Seller.Name +
+                    "<br>&nbsp;Contact Info: " + notification.Seller.Email +
+                    "<br>&nbsp;Address " + notification.Booking.ParkingSpace.Address.Street +
+                    "<br>&nbsp;Amount Paid:" +
+                    "<br>&nbsp;&nbsp;Transaction ID: " + notification.Booking.Id +
+                    "<br>&nbsp;&nbsp;Parking Space Rate: $" + booking.Rate +
+                    $"<br><br>&nbsp;&nbsp; {units} Booked: " + booking.BookingUnits + " " + units +
+                    "<br>&nbsp;&nbsp; Subtotal: $" + booking.Total +
+                    "<br>&nbsp;&nbsp; GST: $" + booking.Total.Value * 0.1m +
+                    "<br>&nbsp;&nbsp; -------------------------------" +
+                    "<br>&nbsp;&nbsp; Total: $" + booking.Total.Value + booking.Total.Value * 0.1m +
+                "<br><br>" +
+                "To manage your bookings head, to your profile page." +
+                "<br><br>" +
+                "Sincerely,<br>" +
+                "The Parkmate Team");
+         } 
     }
 }
